045c089ddb91dc02610b0c2fd159d87b4d3f80fe
bailey-redbrick <67543454+Gayun00@users.noreply.github.com>
2022-07-31 19:54:34 +0900

    Add components and page setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Auth from './pages/Auth';
+import Todo from './pages/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      app
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Todo />} />
+				<Route path='/auth' element={<Auth />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
