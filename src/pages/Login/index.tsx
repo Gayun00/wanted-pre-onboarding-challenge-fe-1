609f22e863b325381e1bf0713b476bb6dc0cef59
bailey-redbrick <67543454+Gayun00@users.noreply.github.com>
2022-08-01 00:10:19 +0900

    Add signup, login validation and api request

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,54 @@
 import React, { useState } from 'react';
 
+interface LoginResponse {
+	message: string;
+	token: string;
+}
+
 function Login() {
 	const [password, setPassword] = useState('');
 	const [email, setEmail] = useState('');
+
+	const validateLogin = () => {
+		const isEmailValid = email.includes('@') || email.includes('.');
+		const isPasswordValid = password.length > 8;
+		return isEmailValid && isPasswordValid;
+	};
+
+	const handleLogin = () => {
+		fetch('http://localhost:8080/users/login', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		}).then((res) => {
+			res.json();
+		});
+	};
+
 	return (
 		<div className='login-wrapper'>
-			<input type='email' name='email' id='' />
-			<input type='password' name='password' id='' />
-			<button>Submit</button>
+			<input
+				type='email'
+				name='email'
+				id=''
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<input
+				type='password'
+				name='password'
+				id=''
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button
+				className={`login_button ${validateLogin() && 'isValid'}`}
+				onClick={handleLogin}>
+				Submit
+			</button>
 		</div>
 	);
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
 function SignUp() {
-	return <div>SignUp</div>;
+	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+
+	const validateSignup = () => {
+		const isEmailValid = email.includes('@') || email.includes('.');
+		const isPasswordValid = password.length > 8;
+		return isEmailValid && isPasswordValid;
+	};
+
+	const handleSignUp = () => {
+		fetch('http://localhost:8080/users/create', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		});
+	};
+
+	return (
+		<div className='signup-wrapper'>
+			<input
+				type='email'
+				name='email'
+				id=''
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<input
+				type='password'
+				name='password'
+				id=''
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button
+				className={`signup_button ${validateSignup() && 'isValid'}`}
+				onClick={handleSignUp}>
+				Submit
+			</button>
+		</div>
+	);
 }
 
 export default SignUp;
