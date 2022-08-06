9e512b2cd5715e81fabee29f2cc699217f079615
bailey-redbrick <67543454+Gayun00@users.noreply.github.com>
2022-08-06 23:11:26 +0900

    Refactor api request

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,60 +1,53 @@
-import {
-	CreateTodoParams,
-	CreateTodoRes,
-	DeleteTodoParams,
-	TodoRes,
-} from 'interfaces/todos';
+import { CreateTodoParams, DeleteTodoParams, TodoRes } from 'interfaces/todos';
 import { SERVER_URL } from 'utils/constants';
 
 const token = localStorage.getItem('token')! as string;
 
-export const fetchTodo = async (): Promise<TodoRes> => {
-	const res = await fetch(`${SERVER_URL}/todos`, {
-		headers: {
-			'Content-type': 'application/json',
-			Authorization: token,
-		},
-	});
-	if (res.ok) {
-		return res.json();
-	}
+const fetchRequest = async (endpoint: string, method: string, params?: any) => {
+	if (params) {
+		const res = await fetch(`${SERVER_URL}${endpoint}`, {
+			method: method,
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify(params),
+		});
+		if (res.ok) {
+			return res.json();
+		}
 
-	throw new Error('Network response not ok');
+		throw new Error('Network response not ok');
+	} else {
+		const res = await fetch(`${SERVER_URL}${endpoint}`, {
+			method: method,
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: token,
+			},
+		});
+		if (res.ok) {
+			return res.json();
+		}
+
+		throw new Error('Network response not ok');
+	}
+};
+
+export const fetchTodo = async (): Promise<TodoRes> => {
+	return fetchRequest('/todos', 'get');
 };
 
 export const createTodo = async (
 	title: string | number | undefined,
 	content: string | number | undefined
 ): Promise<CreateTodoParams> => {
-	const res = await fetch(`${SERVER_URL}/todos`, {
-		method: 'post',
-		headers: {
-			'Content-type': 'application/json',
-			Authorization: token,
-		},
-		body: JSON.stringify({
-			title: title,
-			content: content,
-		}),
+	return fetchRequest('/todos', 'post', {
+		title: title,
+		content: content,
 	});
-	if (res.ok) {
-		return res.json();
-	}
-
-	throw new Error('Network response not ok');
 };
 
 export const deleteTodo = async (id: string): Promise<DeleteTodoParams> => {
-	const res = await fetch(`${SERVER_URL}/todos/${id}`, {
-		method: 'delete',
-		headers: {
-			'Content-type': 'application/json',
-			Authorization: token,
-		},
-	});
-	if (res.ok) {
-		return res.json();
-	}
-
-	throw new Error('Network response not ok');
+	return fetchRequest(`/todos/${id}`, 'delete');
 };
